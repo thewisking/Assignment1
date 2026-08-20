721e5bdff032602a00ab2a8afa0135492274524c
Thewisking <wissamdiab122@gmail.com>
2026-08-20 15:24:32 +0930

    Initial Spring Boot project and homepage. Im giga lost on what to do.

---
+++ src/main/java/COMP3011/Assignment_1/Aissignment1Application.java
@@ -0,0 +1,13 @@
+package COMP3011.Assignment_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aissignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aissignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/COMP3011/Assignment_1/Aissignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package COMP3011.Assignment_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aissignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
