5cf0368e673c7007857ed57302365dde3c8be20e
Thewisking <wissamdiab122@gmail.com>
2026-08-20 19:17:03 +0930

    added the home controller for the home page.

---
+++ src/main/java/COMP3011/Assignment_1/HomeController.java
@@ -0,0 +1,13 @@
+package COMP3011.Assignment_1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";		
+	}
+}
